d65febdf21a6e69de0be56697a6b2365f0bb3e4b
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2024-08-26 14:39:03 -0700

    Update launcher batch file - Add error message if no script found
    Now displays an error if the actual powershell script wasn't found

--- SuperGodMode-EasyLauncher.bat
+++ SuperGodMode-EasyLauncher.bat
@@ -1,7 +1,17 @@
 @echo off
 :: Lines starting with two colons :: are comments
 
-:: Sets the execution policy for the current process to allow the script to run, then runs it
+:: -------------------- If the actual script isn't found, display an error --------------------
+if not exist "Super_God_Mode.ps1" (
+    echo --- Error: The script "Super_God_Mode.ps1" was not found in the current directory ---
+    echo Please ensure the PowerShell script is in the same folder as this batch file. This batch file just launches the script.
+	echo.
+	echo Press any key to exit...
+	pause > nul
+)
+:: --------------------------------------------------------------------------------------------
+
+:: Sets the execution policy for the current process to allow the script to run, then runs it.
 powershell -NoProfile -ExecutionPolicy Bypass -File "Super_God_Mode.ps1"
 
 :: Alternatively, you could run this command yourself in a powershell window before running the script:   		Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
